11783a7d4ba3e7c2c6880af8ad8ca801c3a23930
Muhammad Jawad <muhammad.jawad@devbatch.com>
2022-08-22 17:47:23 +0500

    ALMOOSA-645 removed unnecessary icons

--- src/components/svgIcons/index.js
+++ src/components/svgIcons/index.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
-// import checkSymptoms from './check-sypmtoms-svg.vue'
-// import contactUs from './contact-us-svg.vue'
-// import emergencyConsultationSvg from './emergency-consultation-svg.vue'
-// import healthInsuranceSvg from './health-insurance-svg.vue'
-// import heartCheckupSvg from './heart-checkup-svg.vue'
-// import onSiteConsultationsSvg from './on-site-consultations-svg.vue'
-// import virtualConsultationsSvg from './virtual-consultations-svg.vue'
-// import virtualTourSvg from './virtual-tour-svg.vue'
+
 import newBadgeSvg from './new-badge-svg.vue'
 import usernameSvg from './username-svg.vue'
 import mrnSvg from './mrn-svg.vue'
@@ -19,58 +12,6 @@ import userSvg from './user-svg.vue'
 import lockSvg from './lock-svg.vue'
 import changeCommentSvg from './change-comment-svg.vue'
 import reminderSvg from './reminder-svg.vue'
-
-// Vue.component('check-symptoms-svg', checkSymptoms)
-// Vue.component('contact-us-svg', contactUs)
-// Vue.component('emergency-consultation-svg', emergencyConsultationSvg)
-// Vue.component('health-insurance-svg', healthInsuranceSvg)
-// Vue.component('heart-checkup-svg', heartCheckupSvg)
-// Vue.component('on-site-consultations-svg', onSiteConsultationsSvg)
-// Vue.component('virtual-consultations-svg', virtualConsultationsSvg)
-// Vue.component('virtual-tour-svg', virtualTourSvg)
-Vue.component('new-badge-svg', newBadgeSvg)
-Vue.component('username-svg', usernameSvg)
-Vue.component('mrn-svg', mrnSvg)
-Vue.component('password-svg', passwordSvg)
-Vue.component('location-svg', locationSvg)
-Vue.component('calendar-svg', calendarSvg)
-Vue.component('otp-svg', otpSvg)
-Vue.component('bell-fill-svg', bellFillSvg)
-Vue.component('user-svg', userSvg)
-Vue.component('lock-svg', lockSvg)
-Vue.component('change-comment-svg', changeCommentSvg)
-Vue.component('reminder-svg', reminderSvg)
-
-// import sDashboardSvg from './sidebar/dashboard-svg.vue'
-// import sFindSpecialistSvg from './sidebar/find-specialist-svg.vue'
-// import sHealthEducationSvg from './sidebar/health-education-svg.vue'
-// import sLabWorkSvg from './sidebar/lab-work-svg.vue'
-// import sMedicalInsuranceSvg from './sidebar/medical-insurance-svg.vue'
-// import sMyMedicationSvg from './sidebar/my-medication-svg.vue'
-// import sMyVitalsSvg from './sidebar/my-vitals-svg.vue'
-// import sOnSiteAppointmentsSvg from './sidebar/on-site-appointment-svg.vue'
-// import sPromotionsSvg from './sidebar/promotions-svg.vue'
-// import sRadiologyReportsSvg from './sidebar/radiology-reports-svg.vue'
-// import sSymptomCheckerSvg from './sidebar/symptom-checker-svg.vue'
-// import sViewTimelineSvg from './sidebar/view-timeline-svg.vue'
-// import sVirtualAppointmentsSvg from './sidebar/virtual-appointment-svg.vue'
-// import sVirtualTourSvg from './sidebar/virtual-tour-svg.vue'
-
-// Vue.component('s-dashboard-svg', sDashboardSvg)
-// Vue.component('s-find-specialist-svg', sFindSpecialistSvg)
-// Vue.component('s-health-education-svg', sHealthEducationSvg)
-// Vue.component('s-lab-work-svg', sLabWorkSvg)
-// Vue.component('s-medical-insurance-svg', sMedicalInsuranceSvg)
-// Vue.component('s-my-medication-svg', sMyMedicationSvg)
-// Vue.component('s-my-vitals-svg', sMyVitalsSvg)
-// Vue.component('s-on-site-appointments-svg', sOnSiteAppointmentsSvg)
-// Vue.component('s-promotions-svg', sPromotionsSvg)
-// Vue.component('s-radiology-reports-svg', sRadiologyReportsSvg)
-// Vue.component('s-symptom-checker-svg', sSymptomCheckerSvg)
-// Vue.component('s-view-timeline-svg', sViewTimelineSvg)
-// Vue.component('s-virtual-appointment-svg', sVirtualAppointmentsSvg)
-// Vue.component('s-virtual-tour-svg', sVirtualTourSvg)
-
 import atom from './modules/atom'
 import calendar from './modules/calendar'
 import callHelp from './modules/call-help'
@@ -88,13 +29,23 @@ import symptom from './modules/symptom'
 import timeline from './modules/timeline'
 import userReport from './modules/user-report'
 import userTag from './modules/user-tag'
-
 import dashboardSvg from './modules/dashboard-svg.vue'
-Vue.component('dashboard-svg', dashboardSvg)
 import heartReport from './modules/heart-report-svg.vue'
+
+Vue.component('new-badge-svg', newBadgeSvg)
+Vue.component('username-svg', usernameSvg)
+Vue.component('mrn-svg', mrnSvg)
+Vue.component('password-svg', passwordSvg)
+Vue.component('location-svg', locationSvg)
+Vue.component('calendar-svg', calendarSvg)
+Vue.component('otp-svg', otpSvg)
+Vue.component('bell-fill-svg', bellFillSvg)
+Vue.component('user-svg', userSvg)
+Vue.component('lock-svg', lockSvg)
+Vue.component('change-comment-svg', changeCommentSvg)
+Vue.component('reminder-svg', reminderSvg)
+Vue.component('dashboard-svg', dashboardSvg)
 Vue.component('heart-report-svg', heartReport)
-
-
 Vue.component('atom-svg', atom)
 Vue.component('calendar-svg', calendar)
 Vue.component('call-help-svg', callHelp)
